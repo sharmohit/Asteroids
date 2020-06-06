bafa494207913a202607f4aec3cfc3c1927a88e6
Mohit Sharma <>
2020-06-06 20:50:51 +0530

    Implemented Player Ship mechanics (Movement, Rotation, Shoot, Bound Checking) - Added FPS script for debugging - Added Bullet Prefab and Controller - Added Different Sized Asterioid textures

---
+++ Assets/_Asteroids/Scripts/Game/Bullet/BulletController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Asteroids.Gameplay
+{
+    /// <summary>
+    /// Controls Bullet Behaviour
+    /// </summary>
+    public class BulletController : MonoBehaviour
+    {
+        void Start()
+        {
+            Destroy(gameObject, 2f);
+        }
+
+        void Update()
+        {
+            transform.Translate(Vector3.up * 10f * Time.deltaTime);
+        }
+    }
+}
--- Assets/_Asteroids/Scripts/Game/Player/PlayerController.cs
+++ Assets/_Asteroids/Scripts/Game/Player/PlayerController.cs
@@ -2,24 +2,48 @@
 
 namespace Asteroids.Gameplay
 {
+    /// <summary>
+    /// Controls Player Behaviour
+    /// </summary>
     public class PlayerController : MonoBehaviour
     {
+        [SerializeField] GameObject bullet;
         [SerializeField] float forwardThrust = 7.0f;
-        [SerializeField] float roatationalThrust = 4.0f;
+        [SerializeField] float roatationalThrust = 5.0f;
 
-        private Rigidbody2D rigidbody2D;
+        private Rigidbody2D rb;
+        private Vector2 minBound;
+        private Vector2 maxBound;
+
         private float thrustInput;
         private float rotationalInput;
 
+        private float lastTime;
+
         void Start()
         {
-            rigidbody2D = GetComponent<Rigidbody2D>();
+            rb = GetComponent<Rigidbody2D>();
+
+            CalculateBounds(out minBound, out maxBound);
+
+            lastTime = 0;
         }
 
         void Update()
         {
             rotationalInput = Input.GetAxis("Vertical");
             thrustInput = Input.GetAxis("Horizontal");
+
+            if(Input.GetKeyDown(KeyCode.H))
+            {
+                ActivateHyperspace();
+            }
+
+            if(Input.GetKey(KeyCode.Space) && Time.time > lastTime + 0.25f)
+            {
+                lastTime = Time.time;
+                Instantiate(bullet, transform.position, transform.rotation);
+            }
         }
 
         private void FixedUpdate()
@@ -33,18 +57,48 @@ namespace Asteroids.Gameplay
             if(rotationalInput == 1)
             {
                 Vector2 force = transform.up * forwardThrust;
-                rigidbody2D.AddForce(force);
+                rb.AddForce(force);
             }
         }
 
         private void AddRotationalThrust()
         {
-            rigidbody2D.AddTorque(roatationalThrust * -thrustInput);
+            rb.AddTorque(roatationalThrust * -thrustInput);
         }
 
         private void OnTriggerEnter2D(Collider2D collision)
         {
             Debug.Log(collision.name);
+        }
+
+        private void OnBecameInvisible()
+        {
+            // Bound Check
+            Vector2 newPosition = transform.position;
+            if (transform.position.x < minBound.x)
+                newPosition.x = maxBound.x;
+            if (transform.position.y < minBound.y)
+                newPosition.y = maxBound.y;
+            if (transform.position.x > maxBound.x)
+                newPosition.x = minBound.x;
+            if (transform.position.y > maxBound.y)
+                newPosition.y = minBound.y;
+
+            transform.position = newPosition;
+        }
+
+        private void ActivateHyperspace()
+        {
+            Vector3 randomScreenPoint = new Vector3(Random.value, Random.value, 0);
+            Vector3 randomSpawnPoint = Camera.main.ViewportToWorldPoint(randomScreenPoint);
+            randomSpawnPoint.z = 0;
+            transform.position = randomSpawnPoint;
+        }
+
+        private void CalculateBounds(out Vector2 min, out Vector2 max)
+        {
+            min = Camera.main.ViewportToWorldPoint(new Vector3(0,0,0));
+            max = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, 0));
         }
     }
 }
